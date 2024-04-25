daf9ed28afcf8f4e08fe25ac5adaaaa2ab14ad59
daffa <Daffaizath12@gmail.com>
2024-04-25 17:15:21 +0700

    login register

--- app/src/main/java/com/example/travelapps/LoginActivity.java
+++ app/src/main/java/com/example/travelapps/LoginActivity.java
@@ -1,41 +1,67 @@
 package com.example.travelapps;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.travelapps.Services.ApiServices;
 
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText etEmail, etPassword;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        EditText editTextUsername = findViewById(R.id.editTextUsername);
-        EditText editTextPassword = findViewById(R.id.editTextPassword);
-        Button buttonLogin = findViewById(R.id.buttonLogin);
+        etEmail = findViewById(R.id.et_email);
+        etPassword = findViewById(R.id.et_password);
+    }
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Logika login akan ditambahkan di sini
-                // Contoh: Validasi username dan password
-                String username = editTextUsername.getText().toString();
-                String password = editTextPassword.getText().toString();
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.buttonLogin) {
+            String email = etEmail.getText().toString().trim();
+            String password = etPassword.getText().toString().trim();
+            if (email.isEmpty()) {
+                etEmail.setError("Email tidak boleh kosong");
+            } else if(!isValidEmail(email)){
+                etEmail.setError("Format email tidak valid");
+            } else if (password.isEmpty()){
+                etPassword.setError("Password tidak boleh kosong");
+            } else if (password.length() < 8) {
+                etPassword.setError("Panjang password harus minimal 8 karakter");
+            }  else {
+                ApiServices.login(LoginActivity.this, email, password, new ApiServices.LoginResponseListener() {
+                    @Override
+                    public void onSuccess(String message) {
+                        Toast.makeText(LoginActivity.this, "Berhasil Login", Toast.LENGTH_LONG).show();
+                        Intent i = new Intent(LoginActivity.this, HomeActivity.class);
+                        startActivity(i);
+                        finish();
+                    }
 
-                if (username.equals("example") && password.equals("password")) {
-                    // Login berhasil, pindah ke aktivitas berikutnya
-                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-                    startActivity(intent);
-                    finish(); // Menutup aktivitas login
-                } else {
-                    // Login gagal, tampilkan pesan kesalahan
-                    // Anda dapat menambahkan logika lain sesuai kebutuhan
-                }
+                    @Override
+                    public void onError(String message) {
+                        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_LONG).show();
+                    }
+                });
             }
-        });
+        } else if (v.getId() == R.id.tx_daftar){
+            Intent i = new Intent(this, DaftarActivity.class);
+            startActivity(i);
+        }
+    }
+
+    private boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
     }
 }
---
+++ app/src/main/java/com/example/travelapps/Services/ApiServices.java
@@ -0,0 +1,161 @@
+package com.example.travelapps.Services;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApiServices {
+    private static String HOST = "http://localhost/ProjectTA/api/";
+
+    public interface LoginResponseListener {
+        void onSuccess(String message);
+        void onError(String message);
+    }
+
+    public interface RegisterResponseListener {
+        void onSuccess(String message);
+        void onError(String message);
+    }
+
+    public static void login(Context context, String email, String pass, LoginResponseListener listener) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HOST + "login.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.e("Response", response);
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String message = jsonObject.getString("message");
+                    if (message.equals("Berhasil Login")){
+                        String token = jsonObject.getString("token");
+                        SharedPreferences.Editor editor = context.getSharedPreferences("myPrefs", MODE_PRIVATE).edit();
+                        editor.putBoolean("isLogin", true);
+                        editor.putString("token", token);
+                        editor.apply();
+                        listener.onSuccess(message);
+                    }
+                } catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
+                            try {
+                                int statusCode = error.networkResponse.statusCode;
+                                String responseBody = new String(error.networkResponse.data, "utf-8");
+                                if (statusCode == 401) {
+                                    try {
+                                        JSONObject jsonObject = new JSONObject(responseBody);
+                                        String message = jsonObject.getString("message");
+                                        if (message.equals("Email dan Password Salah")) {
+                                            listener.onError("Email / password anda salah");
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                        listener.onError("Gagal Login: " + e.getMessage());
+                                    }
+
+                                }
+                            }catch (UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        else{
+                            listener.onError("Gagal Login: network response is null");
+
+                        }
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("email", email);
+                params.put("password", pass);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+    public static void register(Context context, String nama, String notelp, String email, String alamat, String password, RegisterResponseListener listener) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, HOST + "register.php", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    String success = jsonObject.getString("success");
+                    if ("true".equals(success)) {
+                        listener.onSuccess("Berhasil Register");
+                    } else {
+                        listener.onError("Gagal register: " + jsonObject.getString("message"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    listener.onError("Gagal register: " + e.getMessage());
+                }
+            }
+        },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse != null && error.networkResponse.data != null) {
+                            try {
+                                int statusCode = error.networkResponse.statusCode;
+                                String responseBody = new String(error.networkResponse.data, "utf-8");
+                                try {
+                                    JSONObject jsonObject = new JSONObject(responseBody);
+                                    String message = jsonObject.getString("message");
+                                    if (message.equals("Email sudah terdaftar")) {
+                                        listener.onError("Email sudah terdaftar , Silahkan gunakan email yang lain");
+                                    }
+                                } catch (JSONException  e) {
+                                    e.printStackTrace();
+                                    listener.onError("Gagal register: " + e.getMessage());
+                                }
+                            } catch ( UnsupportedEncodingException e) {
+                                e.printStackTrace();
+                            }
+
+                        } else {
+                            listener.onError("Gagal register: network response is null");
+                        }
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("nama_lengkap", nama);
+                params.put("email", email);
+                params.put("password", password);
+                params.put("alamat", alamat);
+                params.put("notelp", notelp);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(context);
+        requestQueue.add(stringRequest);
+    }
+
+}
